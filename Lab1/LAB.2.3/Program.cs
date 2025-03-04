da113eb36419fc3891db2e6a5050ebd1eb750065
pro100whp <artemhshs7@gmail.com>
2025-03-04 22:58:29 +0200

    Додали коментарі

--- Lab1/LAB.2.3/Program.cs
+++ Lab1/LAB.2.3/Program.cs
@@ -11,6 +11,8 @@ namespace LAB2._3
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            //создаем словарь с месяцами и количеством дней
             Dictionary<string, int> months = new Dictionary<string, int>();
             months.Add("Січень", 31);
             months.Add("Лютий", 28);
@@ -25,8 +27,12 @@ namespace LAB2._3
             months.Add("Листопад", 30);
             months.Add("Грудень", 31);
 
-            List<string> monthsWith30Days = (List<string>)months.Where(m => m.Value == 30).Select(m => m.Key).ToList();
+            //создаем новый список строк для хранения месяцев у которых 30 дней
+            //из словаря выбираем в начале те елементы у которых значение ровно 30 
+            //у этих елементов берем ключ и помещаем его в наш масив 
+            List<string> monthsWith30Days = months.Where(m => m.Value == 30).Select(m => m.Key).ToList();
 
+            //выводим 
             Console.WriteLine("Місяці, що мають 30 днів:");
             foreach (string month in monthsWith30Days)
             {
