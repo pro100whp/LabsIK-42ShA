ec8ae654764cc0f0040e95c03a043e23e4dfc59d
pro100whp <artemhshs7@gmail.com>
2025-04-12 01:00:24 +0300

    part1

---
+++ lab5/Lab5/Lab5/Building.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+
+    public abstract class Building
+    {
+        public abstract string Address { get; set; }
+        public abstract string BuildingArea { get; set; }
+        public abstract string BuildingDate { get; set; }
+
+        public Building(string buildingDate)
+        {     
+        }
+            
+       
+    }
+    public class PublicBuilding:Building
+    {
+        public override string Address { get; set; }
+        public override string BuildingArea { get; set; }
+        public override string BuildingDate { get; set; }
+
+        public string Ownership {  get; set; }
+        
+        public PublicBuilding(string buildingDate, string ownership):base(buildingDate) 
+        {
+            BuildingDate = buildingDate;
+            Ownership = ownership;
+        }
+    }
+    public class Cinema:PublicBuilding,ICapacity
+    {
+        public string Name {  get; set; }
+        public int NumberOfCinemaHalls { get; set; }
+        public Cinema(string buildingDate, string ownership):base(buildingDate, ownership)
+        {
+            
+        }
+        public int GetCapacity()
+        {
+            //в зале 100 мест 
+            return this.NumberOfCinemaHalls * 100;
+        }
+    }
+    public class Hotel:PublicBuilding,ICapacity
+    {
+        public string Name { get; set; }
+        public int NumberOfRoom { get; set; }
+        public int NumberOfStars { get; set; }  
+        public Hotel(string buildingDate, string ownership) : base(buildingDate, ownership)
+        {
+
+        }
+        public int GetCapacity()
+        {
+            //в отеле все номера 2-х местные
+            return this.NumberOfRoom * 2;
+        }
+    }
+    public class Restaraunt : PublicBuilding,ICapacity
+    {
+        public string Name { get; set; }
+        public int NumberOfSeats { get; set; }
+        public string Cuisine { get; set; }
+        public Restaraunt(string buildingDate, string ownership) : base(buildingDate, ownership)
+        {
+
+        }
+        public int GetCapacity()
+        {
+            
+            return this.NumberOfSeats;
+        }
+    }
+    public class ResidentialBuilding : Building
+    {
+        public override string Address { get; set; }
+        public override string BuildingArea { get; set; }
+        public override string BuildingDate { get; set; }
+
+
+        public ResidentialBuilding(string buildingDate, int numberOfApartments) : base(buildingDate)
+        {
+            BuildingDate = buildingDate;
+        }
+    }
+    public class FiveStoreyBuilding:ResidentialBuilding,ICapacity
+    {
+        public int NumberOfApartments { get; set; }
+
+
+
+        public FiveStoreyBuilding(string buildingDate, int numberOfApartments) : base(buildingDate, numberOfApartments)
+        {
+            BuildingDate = buildingDate;
+        }
+        public int GetCapacity()
+        {
+            //будем считать что в каждой квартире живет в среднем 2 человека
+            return this.NumberOfApartments * 2;
+        }
+    }
+}
---
+++ lab5/Lab5/Lab5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            List<Building> buildings = new List<Building>();
+            
+            Cinema Multiplex = new Cinema("15.02.2010", "Комунальна власність");
+            Multiplex.Name = "Multiplex";
+            Multiplex.NumberOfCinemaHalls = 2;
+            buildings.Add(Multiplex);
+
+            Hotel Eleon = new Hotel("01.01.2007", "Приватна власність");
+            Eleon.Name = "Eleon";
+            Eleon.NumberOfStars = 5;
+            Eleon.NumberOfRoom = 25;
+            buildings.Add(Eleon);
+
+            foreach (Building building in buildings)
+            {
+                if (building is Cinema)
+                {
+                    Cinema cinema = (Cinema)building;
+                    Console.WriteLine($"Кінотеатр '{cinema.Name}' має місткість {cinema.GetCapacity()}");
+                }
+                else if (building is Hotel)
+                {
+                    Hotel hotel = (Hotel)building;
+                    Console.WriteLine($"Готель '{hotel.Name}' має місткість {hotel.GetCapacity()}");
+                }
+            }
+        }
+    }
+}
