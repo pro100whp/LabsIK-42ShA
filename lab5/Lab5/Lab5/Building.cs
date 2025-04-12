7edccee602034383c99a397a12ef1c487274535a
pro100whp <artemhshs7@gmail.com>
2025-04-12 14:03:59 +0300

    part 2

--- lab5/Lab5/Lab5/Building.cs
+++ lab5/Lab5/Lab5/Building.cs
@@ -11,7 +11,7 @@ namespace Lab5
     public abstract class Building
     {
         public abstract string Address { get; set; }
-        public abstract string BuildingArea { get; set; }
+        public abstract double BuildingArea { get; set; }
         public abstract string BuildingDate { get; set; }
 
         public Building(string buildingDate)
@@ -23,7 +23,7 @@ namespace Lab5
     public class PublicBuilding:Building
     {
         public override string Address { get; set; }
-        public override string BuildingArea { get; set; }
+        public override double BuildingArea { get; set; }
         public override string BuildingDate { get; set; }
 
         public string Ownership {  get; set; }
@@ -33,11 +33,16 @@ namespace Lab5
             BuildingDate = buildingDate;
             Ownership = ownership;
         }
+        public override string ToString()
+        {
+            return $"Громадська споруда за адресою:{this.Address}, площею {this.BuildingArea}кв. метрів, збудованою {this.BuildingDate}";
+        }
     }
     public class Cinema:PublicBuilding,ICapacity
     {
         public string Name {  get; set; }
         public int NumberOfCinemaHalls { get; set; }
+        public double PriceOfTicket { get; set; }
         public Cinema(string buildingDate, string ownership):base(buildingDate, ownership)
         {
             
@@ -47,12 +52,21 @@ namespace Lab5
             //в зале 100 мест 
             return this.NumberOfCinemaHalls * 100;
         }
+        public double GetTicketPrice()
+        {
+            return this.PriceOfTicket;
+        }
+        public override string ToString()
+        {
+            return $"Кінотеатр площею {this.BuildingArea}кв. метрів, збудовано {this.BuildingDate}";
+        }
     }
-    public class Hotel:PublicBuilding,ICapacity
+    public class Hotel:PublicBuilding,ICapacity,IRentalRate
     {
         public string Name { get; set; }
         public int NumberOfRoom { get; set; }
         public int NumberOfStars { get; set; }  
+        public double RoomPrice { get; set; }
         public Hotel(string buildingDate, string ownership) : base(buildingDate, ownership)
         {
 
@@ -61,6 +75,14 @@ namespace Lab5
         {
             //в отеле все номера 2-х местные
             return this.NumberOfRoom * 2;
+        }
+        public double GetRentalRate()
+        {
+            return this.RoomPrice;
+        }
+        public override string ToString()
+        {
+            return $"Готель площею {this.BuildingArea}кв. метрів, збудовано {this.BuildingDate}";
         }
     }
     public class Restaraunt : PublicBuilding,ICapacity
@@ -77,33 +99,53 @@ namespace Lab5
             
             return this.NumberOfSeats;
         }
+        public override string ToString()
+        {
+            return $"Ресторан площею {this.BuildingArea}кв. метрів, збудовано {this.BuildingDate}";
+        }
     }
     public class ResidentialBuilding : Building
     {
         public override string Address { get; set; }
-        public override string BuildingArea { get; set; }
+        public override double BuildingArea { get; set; }
         public override string BuildingDate { get; set; }
 
 
-        public ResidentialBuilding(string buildingDate, int numberOfApartments) : base(buildingDate)
+        public ResidentialBuilding(string buildingDate) : base(buildingDate)
         {
             BuildingDate = buildingDate;
         }
+        public override string ToString()
+        {
+            return $"Житлова споруда за адресою:{this.Address}, площею {this.BuildingArea}кв. метрів,збудовано {this.BuildingDate}";
+        }
+        
     }
-    public class FiveStoreyBuilding:ResidentialBuilding,ICapacity
+    public class FiveStoreyBuilding:ResidentialBuilding,ICapacity,IRentalRate
     {
         public int NumberOfApartments { get; set; }
+        public double PricePerSqM { get; set; }
 
 
-
-        public FiveStoreyBuilding(string buildingDate, int numberOfApartments) : base(buildingDate, numberOfApartments)
+        public FiveStoreyBuilding(string buildingDate, int numberOfApartments) : base(buildingDate)
         {
             BuildingDate = buildingDate;
+            NumberOfApartments = numberOfApartments;
+            
         }
         public int GetCapacity()
         {
             //будем считать что в каждой квартире живет в среднем 2 человека
             return this.NumberOfApartments * 2;
         }
+        public override string ToString()
+        {
+            return $"Житловий будинок 5 поверхів за адресою:{this.Address},площею {this.BuildingArea}кв. метрів ,з {this.NumberOfApartments} квартирами, збудовано {this.BuildingDate}";
+        }
+        public  double GetRentalRate()
+        {
+            return this.BuildingArea / this.NumberOfApartments * this.PricePerSqM;
+        }
+
     }
 }
--- lab5/Lab5/Lab5/Program.cs
+++ lab5/Lab5/Lab5/Program.cs
@@ -17,26 +17,67 @@ namespace Lab5
             Cinema Multiplex = new Cinema("15.02.2010", "Комунальна власність");
             Multiplex.Name = "Multiplex";
             Multiplex.NumberOfCinemaHalls = 2;
+            Multiplex.PriceOfTicket = 150;
+            Multiplex.BuildingArea = 2000;
             buildings.Add(Multiplex);
 
             Hotel Eleon = new Hotel("01.01.2007", "Приватна власність");
             Eleon.Name = "Eleon";
             Eleon.NumberOfStars = 5;
             Eleon.NumberOfRoom = 25;
+            Eleon.RoomPrice = 1000;
+            Eleon.BuildingArea = 3000;
             buildings.Add(Eleon);
+
+            Restaraunt Gaga = new Restaraunt("02.03.2020", "Приватна власність");
+            Gaga.Name = "Gaga";
+            Gaga.NumberOfSeats = 50;
+            Gaga.Cuisine = "Georgian";
+            Gaga.BuildingArea = 200;
+            buildings.Add(Gaga);
+
+            FiveStoreyBuilding fiveStoreyBuilding = new FiveStoreyBuilding("30.10.1985", 50);
+            fiveStoreyBuilding.Address = "Kiyv , Zsustret 5";
+            fiveStoreyBuilding.PricePerSqM = 350;
+            fiveStoreyBuilding.BuildingArea = 2500;
+            buildings.Add (fiveStoreyBuilding);
+
+
+
+
 
             foreach (Building building in buildings)
             {
+                //выведем описание обьекта 
+                Console.WriteLine(building.ToString());
                 if (building is Cinema)
                 {
                     Cinema cinema = (Cinema)building;
                     Console.WriteLine($"Кінотеатр '{cinema.Name}' має місткість {cinema.GetCapacity()}");
+                    Console.WriteLine($"Ціна квитка: {cinema.GetTicketPrice()}");
                 }
+
                 else if (building is Hotel)
                 {
                     Hotel hotel = (Hotel)building;
                     Console.WriteLine($"Готель '{hotel.Name}' має місткість {hotel.GetCapacity()}");
+                    Console.WriteLine($"Ціна оренди одного номеру: {hotel.GetRentalRate()} ");
                 }
+
+                else if (building is Restaraunt)
+                {
+                    Restaraunt restaraunt = (Restaraunt)building;
+                    Console.WriteLine($"Ресторан '{restaraunt.Name}' має місткість {restaraunt.GetCapacity()}");
+                    
+                }
+
+                else if (building is FiveStoreyBuilding)
+                {
+                    FiveStoreyBuilding storeyBuilding5 = (FiveStoreyBuilding)building;
+                    Console.WriteLine($"П'ятиповерхівка має місткість {storeyBuilding5.GetCapacity()}");
+                    Console.WriteLine($"Ціна оренди одного помешкання: {storeyBuilding5.GetRentalRate()} ");
+                }
+                Console.WriteLine();
             }
         }
     }
